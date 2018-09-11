3c22dadfd1f4eda71152b4bd5e0dfe3b81fc5bf6
Alex Voloshin <avoloshin225@gmail.com>
2018-09-11 16:29:11 +0300

    Add mongoDB connection

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express');
-
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
+//Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {useMongoClient: true})
+.then(() => console.log('MongoDb Connected ...'))
+.catch(err => console.log(err));
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 //Index route
 app.get('/', (req,res) => {
-  res.send('Hello world');
+  const title = 'Welcome';
+  res.render('index', {title: title});
 });
 
 //About route
 
 app.get('/about', (req,res) => {
-  res.send('About');
+  res.render('about');
 });
 
 const port = 5000;
