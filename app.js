61f426afa82ee05e619b6852b674c604033bbb61
Alex Voloshin <avoloshin225@gmail.com>
2018-09-11 17:45:25 +0300

    Basic CRUD

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 const app = express();
 
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
 //Connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev', {useMongoClient: true})
+mongoose.connect('mongodb://localhost/vidjot-dev', {useNewUrlParser: true})
 .then(() => console.log('MongoDb Connected ...'))
 .catch(err => console.log(err));
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Load Idea Model
+const Idea = require('./models/Idea');
 
 //Handlebars middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
+
+//Method override
+
+app.use(methodOverride('_method'));
 
 //Index route
 app.get('/', (req,res) => {
@@ -25,6 +39,64 @@ app.get('/', (req,res) => {
 
 app.get('/about', (req,res) => {
   res.render('about');
+});
+
+//Ideas index page
+app.get('/ideas', async (req,res) => {
+  const ideas = await Idea.find().sort({date: 'desc'});
+  res.render('ideas/index', {ideas:ideas});
+});
+
+//Add idea form
+app.get('/ideas/add', (req,res) => {
+  res.render('ideas/add');
+});
+
+//Edit idea form
+app.get('/ideas/edit/:id', async (req,res) => {
+  const idea = await Idea.findById(req.params.id);
+  res.render('ideas/edit', {idea:idea});
+});
+
+app.put('/ideas/edit/:id', async (req,res) => {
+  const idea = await Idea.findById(req.params.id);
+  idea.title = req.body.title;
+  idea.details = req.body.details;
+  await idea.save();
+  res.redirect('/ideas');
+});
+
+app.delete('/ideas/delete/:id', async (req,res) => {
+  const idea = await Idea.findById(req.params.id);
+  await idea.remove();
+  res.redirect('/ideas');
+});
+
+// Process Form
+app.post('/ideas', async (req,res) => {
+  let errors = [];
+  if(!req.body.title){
+    errors.push({text: 'Please add a title'});
+  }
+  if(!req.body.details){
+    errors.push({text: 'Please add some details'});
+  } 
+
+  if(errors.length > 0){
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    const idea = new Idea({
+      title: req.body.title,
+      details: req.body.details
+    });
+  
+    await idea.save();
+    res.redirect('/ideas');
+  }
 });
 
 const port = 5000;
